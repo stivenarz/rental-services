3a01a0217771a3682f75629148a1a1d939f94ba1
snarz <ingeariza@live.com>
2025-11-22 01:52:41 -0500

    se elimina json

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,10 @@ import Login from '../components/auth/Login';
 import Navbar from '../components/navbar/Navbar';
 
 export default function LoginPage() {
-    return (
-        <>
-            <Navbar />
-            <Login />
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Login />
+    </>
+  );
 }
